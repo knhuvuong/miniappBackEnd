a6f67d638b45d40e136c07a3528b5bc9307a57f2
YourUsername <your@email.com>
2025-01-21 15:30:42 +0700

    edit

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
-    options: {
-        encrypt: process.env.DB_ENCRYPT === 'true',
-    },
-};
+// const dbConfig = {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     server: process.env.DB_SERVER,
+//     database: process.env.DB_DATABASE,
+//     options: {
+//         encrypt: process.env.DB_ENCRYPT === 'true',
+//     },
+// };
 
 const dbConfigSecond = {
     user: 'sa',
