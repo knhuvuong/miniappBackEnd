abede7aabf1845332283299505579c298a1d2184
YourUsername <your@email.com>
2025-01-21 16:02:10 +0700

    Update code for deployment

--- server.js
+++ server.js
@@ -13,21 +13,11 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// const dbConfig = {
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     server: process.env.DB_SERVER,
-//     database: process.env.DB_DATABASE,
-//     options: {
-//         encrypt: process.env.DB_ENCRYPT === 'true',
-//     },
-// };
-
 const dbConfigSecond = {
-    user: 'sa',
-    password: '123456',
-    server: 'DESKTOP-H2ERSHF',
-    database: 'miniapp',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
     options: {
         encrypt: false,
         trustServerCertificate: true
