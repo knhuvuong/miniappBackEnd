510203f6d89d51110c43a74b09390a2f9982cec5
YourUsername <your@email.com>
2025-03-14 14:43:36 +0700

    fix port

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const dbConfigSecond = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_PORT),
     database: process.env.DB_NAME,
     options: {
         encrypt: false,
